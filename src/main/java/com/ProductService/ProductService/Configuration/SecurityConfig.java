62e823ca6453b51795a6a8f35c27a2d624041427
dhruvgoyal296 <dhruvgoyal2906@gmail.com>
2025-12-06 23:03:43 +0530

    Small Change

--- src/main/java/com/ProductService/ProductService/Configuration/SecurityConfig.java
+++ src/main/java/com/ProductService/ProductService/Configuration/SecurityConfig.java
@@ -9,8 +9,6 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
--- src/main/java/com/ProductService/ProductService/Controller/AuthController.java
+++ src/main/java/com/ProductService/ProductService/Controller/AuthController.java
@@ -11,6 +11,7 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +32,7 @@ public class AuthController {
     @Autowired
     JwtUtil jwtUtil;
 
+    @PostMapping("/login")
     public ResponseEntity<JwtResponse> login (@RequestBody JwtRequest request) {
         try {
             authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
